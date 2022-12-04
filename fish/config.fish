33398c9389fdd9d258f44343611959cbdb791a06
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2022-12-04 15:28:49 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,20 @@
 # Env Variables
+set -u fish_greeting
 set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
 set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 set -Ux OBS_USE_EGL 1
+set -Ux MOZ_ENABLE_WAYLAND 1
+
 
 if status is-interactive
     uwufetch
-   
 end
+
 
 function fish_prompt
     powerline-shell --shell bare $status
 end
+
 
 for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
     if test -d $flatpakdir
