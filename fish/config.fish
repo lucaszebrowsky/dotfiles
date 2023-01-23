6e68f94e7eaad57c75d00b4a50b23bbd1a8ce2b0
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-01-23 20:07:12 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,11 @@
+# Alias
+alias clr="clear"
+
 # Env Variables
 set -u fish_greeting
 set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
 set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-set -Ux OBS_USE_EGL 1
+# set -Ux OBS_USE_EGL 1
 set -Ux MOZ_ENABLE_WAYLAND 1
 
 
@@ -11,13 +14,136 @@ if status is-interactive
 end
 
 
-function fish_prompt
-    powerline-shell --shell bare $status
-end
+#function fish_prompt
+    #powerline-shell --shell bare $status
+#end
 
 
 for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
     if test -d $flatpakdir
         contains $flatpakdir $PATH; or set -a PATH $flatpakdir
     end
+end
+
+
+function fish_prompt
+
+    set -l retc red
+    test $status = 0; and set retc green
+
+    set -q __fish_git_prompt_showupstream
+    or set -g __fish_git_prompt_showupstream auto
+
+    function _nim_prompt_wrapper
+        set retc $argv[1]
+        set -l field_name $argv[2]
+        set -l field_value $argv[3]
+
+        set_color normal
+        set_color 875fff
+        echo -n '─'
+        set_color -o d7afff
+        echo -n '['
+        set_color normal
+        test -n $field_name
+        and echo -n $field_name:
+        set_color $retc
+        set_color -o d7005f
+        echo -n $field_value
+        set_color -o d7afff
+        echo -n ']'
+    end
+
+    set_color -o 875fff #$retc
+    echo -n '┬─'
+    set_color -o d7afff
+    echo -n [
+
+    if functions -q fish_is_root_user; and fish_is_root_user
+        set_color -o red
+    else
+        set_color -o d7005f
+    end
+
+    echo -n $USER
+    set_color -o white
+    echo -n @
+
+    if test -z "$SSH_CLIENT"
+        set_color -o blue
+    else
+        set_color -o cyan
+    end
+
+    echo -n (prompt_hostname)
+    #set_color -o light blue
+    set_color d7005f
+    echo -n :(prompt_pwd)
+    set_color -o d7afff
+    echo -n ']'
+
+    # Date
+    _nim_prompt_wrapper $retc '' (date +%X)
+
+    # Vi-mode
+    # The default mode prompt would be prefixed, which ruins our alignment.
+    function fish_mode_prompt
+    end
+
+    if test "$fish_key_bindings" = fish_vi_key_bindings
+        or test "$fish_key_bindings" = fish_hybrid_key_bindings
+        set -l mode
+        switch $fish_bind_mode
+            case default
+                set mode (set_color --bold red)N
+            case insert
+                set mode (set_color --bold green)I
+            case replace_one
+                set mode (set_color --bold green)R
+                echo '[R]'
+            case replace
+                set mode (set_color --bold cyan)R
+            case visual
+                set mode (set_color --bold magenta)V
+        end
+        set mode $mode(set_color normal)
+        _nim_prompt_wrapper $retc '' $mode
+    end
+
+
+    # Virtual Environment
+    set -q VIRTUAL_ENV_DISABLE_PROMPT
+    or set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    set -q VIRTUAL_ENV
+    and _nim_prompt_wrapper $retc V (basename "$VIRTUAL_ENV")
+
+    # git
+    set -l prompt_git (fish_git_prompt '%s')
+    test -n "$prompt_git"
+    and _nim_prompt_wrapper $retc G $prompt_git
+
+    # Battery status
+    type -q acpi
+    and test (acpi -a 2> /dev/null | string match -r off)
+    and _nim_prompt_wrapper $retc B (acpi -b | cut -d' ' -f 4-)
+
+    # New line
+    echo
+
+    # Background jobs
+    set_color normal
+
+    for job in (jobs)
+        set_color $retc
+        echo -n '│ '
+        set_color brown
+        echo $job
+    end
+
+    set_color normal
+    set_color 875fff
+    echo -n '╰─>'
+    set_color -o d7005f
+    echo -n 'λ  '
+    set_color normal
 end
