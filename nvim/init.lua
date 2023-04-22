a0d46467d19d3f37ea0c79938c38558eb5321c77
Lucas Zebrowsky <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-04-22 15:16:09 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,7 @@ vim.opt.shiftwidth  = 4
 vim.opt.clipboard   = "unnamed"
 vim.opt.background  = "dark"
 vim.opt.termguicolors = true
+vim.opt.cursorline = true
 
 vim.cmd("colorscheme gruvbox")
 vim.cmd("inoremap {<Enter> {<Enter>}<Esc>O")
