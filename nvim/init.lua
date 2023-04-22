7d071339b90aa7970c4ce2bbaa93a8fd38d0d323
Lucas Zebrowsky <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-04-22 10:37:18 +0200

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+vim.opt.belloff     = "all"
+vim.opt.number      = true
+vim.opt.syntax      = "enable"
+vim.opt.autoindent  = true
+vim.opt.tabstop     = 4
+vim.opt.shiftwidth  = 4
+vim.opt.clipboard   = "unnamed"
+vim.opt.background  = "dark"
+vim.opt.termguicolors = true
+
+vim.cmd("colorscheme gruvbox")
+
