db1a855707191781074a3c21af0ca6fe8e4b450e
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-05-25 17:26:51 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,16 @@ vim.opt.cursorline = true
 
 vim.cmd("colorscheme gruvbox")
 
+vim.g.neoformat_enabled = { 'clangformat' }
+
+-- Autoformat on save
+vim.api.nvim_exec([[
+    augroup AutoFormat
+        autocmd!
+        autocmd BufWritePre * Neoformat
+    augroup end
+]], true)
+
 -- Autocompletion
 -- vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true })
 -- vim.api.nvim_set_keymap('i', '<CR>', [[getline('.')[col('.')-2] == '{' ? '<CR><Esc>O<BS><Tab>' : '<CR>']], { noremap = true, expr = true })
