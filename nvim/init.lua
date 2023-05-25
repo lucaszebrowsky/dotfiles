5de01848d31fe9b2a326a6b3d4fec68af7ad8468
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-05-25 09:26:48 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,8 +12,10 @@ vim.opt.cursorline = true
 vim.cmd("colorscheme gruvbox")
 
 -- Autocompletion
-vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true })
-vim.api.nvim_set_keymap('i', '<CR>', [[getline('.')[col('.')-2] == '{' ? '<CR><Esc>O<BS><Tab>' : '<CR>']], { noremap = true, expr = true })
-vim.api.nvim_set_keymap('i', '(', '()<Left>', { noremap = true })
-vim.api.nvim_set_keymap('i', '"', '""<Left>', { noremap = true })
-vim.api.nvim_set_keymap('i', '`', '``<Left>', { noremap = true })
+-- vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true })
+-- vim.api.nvim_set_keymap('i', '<CR>', [[getline('.')[col('.')-2] == '{' ? '<CR><Esc>O<BS><Tab>' : '<CR>']], { noremap = true, expr = true })
+-- vim.api.nvim_set_keymap('i', '(', '()<Left>', { noremap = true })
+-- vim.api.nvim_set_keymap('i', '"', '""<Left>', { noremap = true })
+-- vim.api.nvim_set_keymap('i', '`', '``<Left>', { noremap = true })
+
+require("plugins")
