bc3e4d8f2c1a86c02df368cd3232dd37ea8d38cc
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-06-07 09:15:20 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,21 @@
 # Alias
 alias clr="clear"
 alias vim="nvim"
+alias ssh="kitty +kitten ssh"
+
+# Workaround for using sudo + vim alias:
+# create a symlink called "vim" to "nvim"
+# ln -s /usr/bin/nvim /usr/bin/vim
 
 # Env Variables
 set -u fish_greeting
 set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
 set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-# set -Ux OBS_USE_EGL 1
 set -Ux MOZ_ENABLE_WAYLAND 1
 
-
 if status is-interactive
-	neofetch
+    neofetch
 end
-
 
 for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
     if test -d $flatpakdir
