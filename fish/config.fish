a3ab9e1b45443092a93d64beba83cef70b23992e
Lucas Zebrowsky <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2023-12-19 15:34:25 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,10 @@
 # Alias
 alias clr="clear"
-# alias vim="nvim"
-# alias ssh="kitty +kitten ssh"
-# alias hx="helix"
-
-# Workaround for using sudo + vim alias:
-# create a symlink called "vim" to "nvim"
-# ln -s /usr/bin/nvim /usr/bin/vim
+alias ..="cd .."
 
 # Env Variables
 set -u fish_greeting
-set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
-set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
-set -Ux MOZ_ENABLE_WAYLAND 1
+set -Ux GTK_USE_PORTAL 1
 
 if status is-interactive
     if not set -q TMUX
@@ -21,12 +13,6 @@ if status is-interactive
         tmux attach-session -d -t main
     end
     neofetch    
-end
-
-for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
-    if test -d $flatpakdir
-        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
-    end
 end
 
 # bun
