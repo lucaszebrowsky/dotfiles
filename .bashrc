931159dac54b490e40d9dad41b460fc18646da4d
Lu-Die-Milchkuh <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2024-01-02 22:17:39 +0100

    Uploaded som changes I forgot I made

---
+++ .bashrc
@@ -0,0 +1,9 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Bash is just a bootloader for fish lol
+fish
--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,12 @@
 # Alias
 alias clr="clear"
 alias ..="cd .."
+alias la="ls -la --color=auto"
+alias ll="ls -l --color=auto"
+alias grep='grep --color=auto'
 
 # Env Variables
-set -u fish_greeting
+set -U fish_greeting
 set -Ux GTK_USE_PORTAL 1
 
 if status is-interactive
