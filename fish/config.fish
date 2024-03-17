7f2184687eb37da80320214465e73b66eb7dfb4f
Lucas Zebrowsky <78442275+Lu-Die-Milchkuh@users.noreply.github.com>
2024-03-17 14:12:50 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,13 +9,12 @@ alias grep='grep --color=auto'
 set -U fish_greeting
 set -Ux GTK_USE_PORTAL 1
 
+if type -q helix
+	alias hx="helix"
+end
+
 if status is-interactive
-    if not set -q TMUX
-        set -g TMUX tmux new-session -d -s main
-        eval $TMUX
-        tmux attach-session -d -t main
-    end
-    neofetch    
+   neofetch    
 end
 
 # bun
