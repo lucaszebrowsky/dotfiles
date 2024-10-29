5e70adc217f24c2be04065b6edb9c9263eba2a96
Lu-Die-Milchkuh <zebrowskylucas@proton.me>
2024-10-29 15:19:51 +0100

    Updated Fish Shell

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-    string join "" (set_color green) "$USER" (set_color normal) "@" (set_color red) (prompt_hostname) (set_color normal) ":" (set_color blue) "$PWD" (set_color normal) \n "\$ "
+    string join "" (set_color green) "$USER" (set_color normal) "@" (set_color red) (prompt_hostname) (set_color normal) ":" (set_color blue) (prompt_pwd) (set_color normal) \n "\$ "
 end
