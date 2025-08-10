7da943e3a8e9f6848c257ac0d689981673682b2e
Lucas Zebrowsky <zebrowskylucas@proton.me>
2025-08-10 16:15:30 +0200

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -3,18 +3,28 @@ alias clr="clear"
 alias ..="cd .."
 alias la="ls -la --color=auto"
 alias ll="ls -l --color=auto"
-alias grep='grep --color=auto'
+alias grep="grep --color=auto"
+alias t="tree ."
+alias uptime="uptime -p"
+
+# Git
+alias gs="git status"
+alias gd="git diff"
+alias gc="git commit"
+alias gp="git push"
 
 # Env Variables
 set -U fish_greeting
 set -Ux GTK_USE_PORTAL 1
 
 if status is-interactive
-    if test $TERM = xterm-kitty
-        alias ssh="kitten ssh"
-    end
-end
+    # if test $TERM = xterm-kitty
+    #     alias ssh="kitten ssh"
+    # end
 
-# bun
-# set --export BUN_INSTALL "$HOME/.bun"
-# set --export PATH $BUN_INSTALL/bin $PATH
+    # Use Helix as default terminal editor
+    if type -q hx
+        set -Ux EDITOR $(which hx)
+    end
+
+end
