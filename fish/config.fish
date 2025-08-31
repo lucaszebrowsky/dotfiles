d4c3b5cc6b4d7fec795dd6a6698ae6bab1e23857
Lucas Zebrowsky <zebrowskylucas@proton.me>
2025-08-31 09:47:38 +0200

    Add tmux alias and set the default shs-agent to OpenSSH's build-in one on Linux

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,10 @@ alias grep="grep --color=auto"
 alias t="tree ."
 alias uptime="uptime -p"
 
+# Tmux
+alias ta="tmux attach -t"
+alias tls="tmux list-session"
+
 # Git
 alias gs="git status"
 alias gd="git diff"
@@ -20,12 +24,16 @@ alias get_idf=". ~/.esp/esp-idf/export.fish"
 # Env Variables
 set -U fish_greeting
 set -Ux GTK_USE_PORTAL 1
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+# Use OpenSSH's build-in ssh-agent
+# Enable via: systemctl enable --user --now ssh-agent.socket
+if test -n "$XDG_RUNTIME_DIR" -a (uname) = Linux
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+end
 
 if status is-interactive
     # Use Helix as default terminal editor
     if type -q hx
         set -Ux EDITOR $(which hx)
     end
-
 end
